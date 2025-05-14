d71cd62021b3aac8dab684ba60b044cf029ec9b2
Bakasa <baksajoshua09@gmail.com>
2025-05-14 14:11:56 +0300

    Added the application layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./components/sidebar/sidebar/sidebar.component";
+import { PageLayoutComponent } from "./components/page-layout/page-layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SidebarComponent, PageLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-template';
+  title = 'Angular Tailwind Template';
 }
