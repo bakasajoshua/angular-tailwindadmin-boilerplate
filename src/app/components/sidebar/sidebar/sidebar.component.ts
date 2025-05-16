370e6af3299093ec3808a3f7cf88a418dc792ba3
Bakasa <baksajoshua09@gmail.com>
2025-05-16 14:40:33 +0300

    Side bar component added

--- src/app/components/sidebar/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar/sidebar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { AccountToogleComponent } from "../account-toogle/account-toogle.component";
+import { SearchComponent } from "../search/search.component";
+import { SideMenuComponent } from "../side-menu/side-menu.component";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [AccountToogleComponent],
+  imports: [AccountToogleComponent, SearchComponent, SideMenuComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
