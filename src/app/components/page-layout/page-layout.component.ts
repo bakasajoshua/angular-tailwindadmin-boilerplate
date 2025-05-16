6335a32ee8fa36a2dcae81f084c0fb82dde0cadb
Bakasa <baksajoshua09@gmail.com>
2025-05-16 14:41:00 +0300

    Added the bare page layout

---
+++ src/app/components/page-layout/page-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TopBarComponent } from "./top-bar/top-bar.component";
+
+@Component({
+  selector: 'app-page-layout',
+  imports: [TopBarComponent],
+  templateUrl: './page-layout.component.html',
+  styleUrl: './page-layout.component.css'
+})
+export class PageLayoutComponent {
+
+}
