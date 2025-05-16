8491a3edfc5dab2dff34b69e84e591f5130a7fdb
Bakasa <baksajoshua09@gmail.com>
2025-05-16 14:38:38 +0300

    Added the command menu component - Still not in use

---
+++ src/app/components/sidebar/command-menu/command-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-command-menu',
+  imports: [],
+  templateUrl: './command-menu.component.html',
+  styleUrl: './command-menu.component.css'
+})
+export class CommandMenuComponent {
+
+}
