c7b1f761442e8b38ec34579fcc009c0a6243dd70
Bakasa <baksajoshua09@gmail.com>
2025-05-20 14:43:22 +0300

    Added placeholders for the activity and usage charts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { StatsCardComponent } from "./stats/stats-card/stats-card.component";
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { featherUser, featherEye } from '@ng-icons/feather-icons';
+import { ActivityGraphComponent } from "./activity-graph/activity-graph.component";
+import { UsageGraphComponent } from './usage-graph/usage-graph.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [StatsCardComponent],
+  imports: [StatsCardComponent, NgIcon, ActivityGraphComponent, UsageGraphComponent],
+  providers: [provideIcons({ featherUser, featherEye })],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
