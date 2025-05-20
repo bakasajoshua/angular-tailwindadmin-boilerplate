527eb3fba4b0e330cc5dba1537186d899ed7d691
Bakasa <baksajoshua09@gmail.com>
2025-05-20 14:44:07 +0300

    Generated the usage chart

---
+++ src/app/pages/dashboard/usage-graph/usage-graph.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usage-graph',
+  imports: [],
+  templateUrl: './usage-graph.component.html',
+  styleUrl: './usage-graph.component.css'
+})
+export class UsageGraphComponent {
+
+}
