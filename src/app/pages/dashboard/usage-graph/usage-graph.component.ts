8ce3e5eb05f3481983d311d309e284ffe45826e5
bakasajoshua <baksajoshua09@gmail.com>
2025-05-21 02:44:29 +0300

    Adding the usage pie chart

--- src/app/pages/dashboard/usage-graph/usage-graph.component.ts
+++ src/app/pages/dashboard/usage-graph/usage-graph.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { NgxEchartsDirective } from 'ngx-echarts';
+import { EChartsCoreOption } from 'echarts/core';
 
 @Component({
   selector: 'app-usage-graph',
-  imports: [],
+  imports: [NgxEchartsDirective],
   templateUrl: './usage-graph.component.html',
   styleUrl: './usage-graph.component.css'
 })
 export class UsageGraphComponent {
-
+  chartOption: EChartsCoreOption = {
+    title: {
+      text: 'Referer of a Website',
+      subtext: 'Fake Data',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: '50%',
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
 }
