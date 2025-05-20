cbccdee039d40cefda77a6b59ea6c935cbb3e077
Bakasa <baksajoshua09@gmail.com>
2025-05-20 10:04:20 +0300

    Added reference to the dashboard component on the main layout

--- src/app/components/page-layout/page-layout.component.ts
+++ src/app/components/page-layout/page-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TopBarComponent } from "./top-bar/top-bar.component";
+import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-page-layout',
-  imports: [TopBarComponent],
+  imports: [TopBarComponent, DashboardComponent],
   templateUrl: './page-layout.component.html',
   styleUrl: './page-layout.component.css'
 })
