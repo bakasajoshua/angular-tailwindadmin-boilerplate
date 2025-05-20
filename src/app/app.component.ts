0bafde302f3e67fa5a204506119b4c73739d4832
Bakasa <baksajoshua09@gmail.com>
2025-05-20 14:42:12 +0300

    Added e-charts master imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+import { LineChart } from 'echarts/charts';
+import { GridComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
 import { SidebarComponent } from "./components/sidebar/sidebar/sidebar.component";
 import { PageLayoutComponent } from "./components/page-layout/page-layout.component";
+
+
+echarts.use([LineChart, GridComponent, CanvasRenderer]);
 
 @Component({
   selector: 'app-root',
   imports: [SidebarComponent, PageLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+      provideEchartsCore({ echarts }),
+    ],
 })
 export class AppComponent {
   title = 'Angular Tailwind Template';
